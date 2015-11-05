cdbd4af935e4d5866d0c3937bdd5354489c2de10
Tz <512932926@qq.com>
2015-11-06 00:13:27 +0800

    test

--- js/hux-blog.js
+++ js/hux-blog.js
@@ -35,34 +35,35 @@ $(document).ready(function () {
 });
 
 // Navigation Scripts to Show Header on Scroll-Up
-//jQuery(document).ready(function($) {
-//    var MQL = 1170;
+jQuery(document).ready(function($) {
+    var MQL = 1170;
 
     //primary navigation slide-in effect
-    $(window).ready(function(){$(window).scroll( function() { alert("ss") }); });
+
 
     
-//    if ($(window).width() > MQL) {
-//        var headerHeight = $('.navbar-custom').height();
-//        $(window).on('scroll', {
-//                previousTop: 0
-//            },
-//            function() {
-//                var currentTop = $(window).scrollTop();
-//                //check if user is scrolling up
-//                if (currentTop < this.previousTop) {
-//                    //if scrolling up...
-//                    if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
-//                        $('.navbar-custom').addClass('is-visible');
-//                    } else {
-//                        $('.navbar-custom').removeClass('is-visible is-fixed');
-//                    }
-//                } else {
-//                    //if scrolling down...
+    if ($(window).width() > MQL) {
+        var headerHeight = $('.navbar-custom').height();
+        $(window).on('scroll', {
+                previousTop: 0
+            },
+            function() {
+                var currentTop = $(window).scrollTop();
+                //check if user is scrolling up
+                if (currentTop < this.previousTop) {
+                    //if scrolling up...
+                    if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
+                        $('.navbar-custom').addClass('is-visible');
+                    } else {
+                        $('.navbar-custom').removeClass('is-visible is-fixed');
+                    }
+                } else {
+                    //if scrolling down...
+                    $('.navbar-custom').addClass('is-visible is-fixed');
 //                    $('.navbar-custom').removeClass('is-visible');
 //                    if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
-//                }
-//                this.previousTop = currentTop;
-//            });
-//    }
-//});
+                }
+                this.previousTop = currentTop;
+            });
+    }
+});
