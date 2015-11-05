1a1983ab5e1e8f50c4383ba30411eef03808160d
Tz <512932926@qq.com>
2015-11-06 00:16:43 +0800

    finally test

--- js/hux-blog.js
+++ js/hux-blog.js
@@ -48,22 +48,22 @@ jQuery(document).ready(function($) {
                 previousTop: 0
             },
             function() {
-                var currentTop = $(window).scrollTop();
-                //check if user is scrolling up
-                if (currentTop < this.previousTop) {
-                    //if scrolling up...
-                    if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
-                        $('.navbar-custom').addClass('is-visible');
-                    } else {
-                        $('.navbar-custom').removeClass('is-visible is-fixed');
-                    }
-                } else {
-                    //if scrolling down...
-                    $('.navbar-custom').addClass('is-visible is-fixed');
+            $('.navbar-custom').addClass('is-visible');
+//                var currentTop = $(window).scrollTop();
+//                //check if user is scrolling up
+//                if (currentTop < this.previousTop) {
+//                    //if scrolling up...
+//                    if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
+//                        $('.navbar-custom').addClass('is-visible');
+//                    } else {
+//                        $('.navbar-custom').removeClass('is-visible is-fixed');
+//                    }
+//                } else {
+//                    //if scrolling down...
 //                    $('.navbar-custom').removeClass('is-visible');
 //                    if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
-                }
-                this.previousTop = currentTop;
+//                }
+//                this.previousTop = currentTop;
             });
     }
 });
