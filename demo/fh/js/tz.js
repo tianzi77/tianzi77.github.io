296dd75acc76b1f73553e7a999e1bc37cbeeed0d
weiwzhang <weiwzhang@Ctrip.com>
2016-01-11 15:06:55 +0800

    最终版本

--- demo/fh/js/tz.js
+++ demo/fh/js/tz.js
@@ -24,26 +24,67 @@ $(function () {
             $(".nav").removeClass('nav-fixed');
         }
 
-//        var tt = $(window).height() - 36; 
-//        var num = 0;
-//        for (var n = 0; n < 6; n++) {
-//            if (top >= n * tt && top <= (n + 1) * tt) {
-//                num = n;
-//
-//            }
-//            $('.nav li a').removeClass("selected").eq(num).addClass("selected");
-//        }
+        var f1, f2, f3, f4, f5, f6, f7, bck;
+        var fixTop = $('.nav a');
+        fl = $('#info').offset().top;
 
+        f2 = $('#schedule').offset().top;
+
+        f3 = $('#guest').offset().top;
+
+        f4 = $('#cooperation').offset().top;
+
+        f5 = $('#map').offset().top;
+
+        f6 = $('#review').offset().top;
+
+        f7 = $('#gift').offset().top;
+
+        if (top >= fl) {
+
+            fixTop.eq(0).addClass("selected").parents().siblings().find("a").removeClass("selected");
+
+        }
+
+        if (top >= f2 - 100) {
+
+            fixTop.eq(1).addClass("selected").parents().siblings().find("a").removeClass("selected");
+
+        }
+
+        if (top >= f3 - 100) {
+
+            fixTop.eq(2).addClass("selected").parents().siblings().find("a").removeClass("selected");
+
+        }
+
+        if (top >= f4 - 100) {
+
+            fixTop.eq(3).addClass("selected").parents().siblings().find("a").removeClass("selected");
+
+        }
+
+        if (top >= f5 - 100) {
+
+            fixTop.eq(4).addClass("selected").parents().siblings().find("a").removeClass("selected");
+
+        }
+
+        if (top >= f6 - 100) {
+
+            fixTop.eq(5).addClass("selected").parents().siblings().find("a").removeClass("selected");
+
+        }
     });
 
     $(".footer-nav").on("click", function () {
         $(".nav a").removeClass('selected');
         $(".nav li:nth-child(1)>a").addClass('selected');
-    })
+    });
     $(".footer-nav1").on("click", function () {
         $(".nav a").removeClass('selected');
         $(".nav li:nth-child(3)>a").addClass('selected');
-    })
+    });
     $(".footer-nav2").on("click", function () {
             $(".nav a").removeClass('selected');
             $(".nav li:nth-child(4)>a").addClass('selected');
@@ -67,15 +108,15 @@ $(function () {
     $('.tan-box').on("click", function () {
             $('#overlay').show();
             $('.yilongCeo').show();
-//            $('body').addClass("fix-scroll");
-//            document.documentElement.style.overflow = 'hidden';
-//            document.body.style.overflow = 'hidden'; //手机版
+            //            $('body').addClass("fix-scroll");
+            //            document.documentElement.style.overflow = 'hidden';
+            //            document.body.style.overflow = 'hidden'; //手机版
         })
         //close 
     $('.close').on('click', function () {
         $('.guest-box').hide();
         $('#overlay').hide();
-//        document.documentElement.style.overflow = 'scroll';
-//        document.body.style.overflow = 'scroll'; //手机版
+        //        document.documentElement.style.overflow = 'scroll';
+        //        document.body.style.overflow = 'scroll'; //手机版
     })
 })
