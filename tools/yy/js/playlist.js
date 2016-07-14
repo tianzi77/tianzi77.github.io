16040702f1d6635dbe8e6e2daaae0e07cb4c8290
weiwzhang <weiwzhang@Ctrip.com>
2016-07-14 17:52:22 +0800

    音乐测试

---
+++ tools/yy/js/playlist.js
@@ -0,0 +1,89 @@
+{
+    "playlist": [
+        {
+            "0": {
+                "src": "http://mp3.haoduoge.com/s/2016-06-06/1465150093.mp3",
+                "type": "audio/mp3"
+            },
+            "config": {
+                "title": "haha",
+                "poster": "http://www.zhangweiwei.cn/img/logo.png"
+            }
+	},
+        {
+            "0": {
+                "src": "http://mp3.haoduoge.com/s/2016-07-08/1467980593.mp3",
+                "type": "audio/mp3"
+            },
+            "config": {
+                "title": "haha2",
+                "poster": "http://www.zhangweiwei.cn/img/logo.png"
+            }
+	},
+        {
+            "0": {
+                "src": "http://mp3.haoduoge.com/s/2016-07-08/1467980593.mp3",
+                "type": "audio/mp3"
+            },
+            "config": {
+                "title": "haha2",
+                "poster": "http://www.zhangweiwei.cn/img/logo.png"
+            }
+	},
+         {
+            "0": {
+                "src": "http://mp3.haoduoge.com/s/2016-07-08/1467980593.mp3",
+                "type": "audio/mp3"
+            },
+            "config": {
+                "title": "haha2",
+                "poster": "http://www.zhangweiwei.cn/img/logo.png"
+            }
+	}, {
+            "0": {
+                "src": "http://mp3.haoduoge.com/s/2016-07-08/1467980593.mp3",
+                "type": "audio/mp3"
+            },
+            "config": {
+                "title": "haha2",
+                "poster": "http://www.zhangweiwei.cn/img/logo.png"
+            }
+	}, {
+            "0": {
+                "src": "http://mp3.haoduoge.com/s/2016-07-08/1467980593.mp3",
+                "type": "audio/mp3"
+            },
+            "config": {
+                "title": "haha2",
+                "poster": "http://www.zhangweiwei.cn/img/logo.png"
+            }
+	}, {
+            "0": {
+                "src": "http://mp3.haoduoge.com/s/2016-07-08/1467980593.mp3",
+                "type": "audio/mp3"
+            },
+            "config": {
+                "title": "haha2",
+                "poster": "http://www.zhangweiwei.cn/img/logo.png"
+            }
+	}, {
+            "0": {
+                "src": "http://mp3.haoduoge.com/s/2016-07-08/1467980593.mp3",
+                "type": "audio/mp3"
+            },
+            "config": {
+                "title": "haha2",
+                "poster": "http://www.zhangweiwei.cn/img/logo.png"
+            }
+	}, {
+            "0": {
+                "src": "http://mp3.haoduoge.com/s/2016-07-08/1467980593.mp3",
+                "type": "audio/mp3"
+            },
+            "config": {
+                "title": "haha2",
+                "poster": "http://www.zhangweiwei.cn/img/logo.png"
+            }
+	}
+    ]
+}
