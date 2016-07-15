7ccf6a75f9d41ec75d450feec61da245589d7634
weiwzhang <weiwzhang@Ctrip.com>
2016-07-15 16:59:52 +0800

    新增歌曲

--- tools/yy/js/playlist.js
+++ tools/yy/js/playlist.js
@@ -2,12 +2,12 @@
     "playlist": [
         {
             "0": {
-                "src": "http://mp3.haoduoge.com/s/2016-06-06/1465150093.mp3",
+                "src": "http://u.jq.cc/u/streamrdt2/2941.adcaf3b8cbbc148d63c8e979efd98c66/01468572737/2941.adcaf3b8cb.mp3",
                 "type": "audio/mp3"
             },
             "config": {
-                "title": "歌曲测试",
-                "poster": "http://www.zhangweiwei.cn/img/logo.png"
+                "title": "别怕--MC小洲",
+                "poster": "http://www.zhangweiwei.cn/tools/yy/img/bp.jpg"
             }
 	},
         {
