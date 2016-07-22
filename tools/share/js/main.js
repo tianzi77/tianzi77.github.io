7aecccd3296b7942579530bb724db14003a42415
weiwzhang <weiwzhang@Ctrip.com>
2016-07-22 13:42:41 +0800

    测试活动

--- tools/share/js/main.js
+++ tools/share/js/main.js
@@ -1,110 +1,129 @@
 // canvas 
-(function () {
-    var COLORS, Confetti, NUM_CONFETTI, PI_2, canvas, confetti, context, drawCircle, i, range, resizeWindow, xpos;
+            (function() {
+                var COLORS, Confetti, NUM_CONFETTI, PI_2, canvas, confetti, context, drawCircle, i, range, resizeWindow, xpos;
 
-    NUM_CONFETTI = 350;
+                NUM_CONFETTI = 350;
 
-    COLORS = [[85, 71, 106], [174, 61, 99], [219, 56, 83], [244, 92, 68], [248, 182, 70]];
+                COLORS = [
+                    [85, 71, 106],
+                    [174, 61, 99],
+                    [219, 56, 83],
+                    [244, 92, 68],
+                    [248, 182, 70]
+                ];
 
-    PI_2 = 2 * Math.PI;
+                PI_2 = 2 * Math.PI;
 
-    canvas = document.getElementById("flower");
+                canvas = document.getElementById("flower");
 
-    context = canvas.getContext("2d");
+                if (canvas != undefined && canvas.getContext != undefined) {
 
-    window.w = canvas.width = 1180;
+                context = canvas.getContext("2d");
 
-    window.h = canvas.height = 447;
+                window.w = 0;
 
-    range = function (a, b) {
-        return (b - a) * Math.random() + a;
-    };
+                window.h = 0;
 
-    drawCircle = function (x, y, r, style) {
-        context.beginPath();
-        context.arc(x, y, r, 0, PI_2, false);
-        context.fillStyle = style;
-        return context.fill();
-    };
 
-    xpos = 0.5;
+                resizeWindow = function() {
+                    window.w = canvas.width = document.getElementById("header").offsetWidth;
+                    window.h = canvas.height = document.getElementById("header").offsetHeight;
+                };
 
-    document.onmousemove = function (e) {
-        return xpos = e.pageX / w;
-    };
-
-    window.requestAnimationFrame = (function () {
-        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
-            return window.setTimeout(callback, 1000 / 600);
-        };
-    })();
-
-    Confetti = (function () {
-        function Confetti() {
-            this.style = COLORS[~~range(0, 5)];
-            this.rgb = "rgba(" + this.style[0] + "," + this.style[1] + "," + this.style[2];
-            this.r = ~~range(2, 6);
-            this.r2 = 2 * this.r;
-            this.replace();
-        }
-
-        Confetti.prototype.replace = function () {
-            this.opacity = 0;
-            this.dop = 0.03 * range(1, 4);
-            this.x = range(-this.r2, w - this.r2);
-            this.y = range(-20, h - this.r2);
-            this.xmax = w - this.r;
-            this.ymax = h - this.r;
-            this.vx = range(0, 2) + 8 * xpos - 5;
-            return this.vy = 0.7 * this.r + range(-1, 1);
-        };
-
-        Confetti.prototype.draw = function () {
-            var ref;
-            this.x += this.vx;
-            this.y += this.vy;
-            this.opacity += this.dop;
-            if (this.opacity > 1) {
-                this.opacity = 1;
-                this.dop *= -1;
+                window.addEventListener('load', resizeWindow, false);
+            }else{
+                return;
             }
-            if (this.opacity < 0 || this.y > this.ymax) {
-                this.replace();
-            }
-            if (!((0 < (ref = this.x) && ref < this.xmax))) {
-                this.x = (this.x + this.xmax) % this.xmax;
-            }
-            return drawCircle(~~this.x, ~~this.y, this.r, this.rgb + "," + this.opacity + ")");
-        };
 
-        return Confetti;
+                range = function(a, b) {
+                    return (b - a) * Math.random() + a;
+                };
 
-    })();
+                drawCircle = function(x, y, r, style) {
+                    context.beginPath();
+                    context.arc(x, y, r, 0, PI_2, false);
+                    context.fillStyle = style;
+                    return context.fill();
+                };
 
-    confetti = (function () {
-        var j, ref, results;
-        results = [];
-        for (i = j = 1, ref = NUM_CONFETTI; 1 <= ref ? j <= ref : j >= ref; i = 1 <= ref ? ++j : --j) {
-            results.push(new Confetti);
-        }
-        return results;
-    })();
+                xpos = 0.5;
 
-    window.step = function () {
-        var c, j, len, results;
-        requestAnimationFrame(step);
-        context.clearRect(0, 0, w, h);
-        results = [];
-        for (j = 0, len = confetti.length; j < len; j++) {
-            c = confetti[j];
-            results.push(c.draw());
-        }
-        return results;
-    };
+                document.onmousemove = function(e) {
+                    return xpos = e.pageX / w;
+                };
 
-    step();
+                window.requestAnimationFrame = (function() {
+                    return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(callback) {
+                        return window.setTimeout(callback, 1000 / 60);
+                    };
+                })();
 
-}).call(this);
+                Confetti = (function() {
+                    function Confetti() {
+                        this.style = COLORS[~~range(0, 5)];
+                        this.rgb = "rgba(" + this.style[0] + "," + this.style[1] + "," + this.style[2];
+                        this.r = ~~range(2, 6);
+                        this.r2 = 2 * this.r;
+                        this.replace();
+                    }
+
+                    Confetti.prototype.replace = function() {
+                        this.opacity = 0;
+                        this.dop = 0.03 * range(1, 4);
+                        this.x = range(-this.r2, w - this.r2);
+                        this.y = range(-20, h - this.r2);
+                        this.xmax = w - this.r;
+                        this.ymax = h - this.r;
+                        this.vx = range(0, 2) + 8 * xpos - 5;
+                        return this.vy = 0.7 * this.r + range(-1, 1);
+                    };
+
+                    Confetti.prototype.draw = function() {
+                        var ref;
+                        this.x += this.vx;
+                        this.y += this.vy;
+                        this.opacity += this.dop;
+                        if (this.opacity > 1) {
+                            this.opacity = 1;
+                            this.dop *= -1;
+                        }
+                        if (this.opacity < 0 || this.y > this.ymax) {
+                            this.replace();
+                        }
+                        if (!((0 < (ref = this.x) && ref < this.xmax))) {
+                            this.x = (this.x + this.xmax) % this.xmax;
+                        }
+                        return drawCircle(~~this.x, ~~this.y, this.r, this.rgb + "," + this.opacity + ")");
+                    };
+
+                    return Confetti;
+
+                })();
+
+                confetti = (function() {
+                    var j, ref, results;
+                    results = [];
+                    for (i = j = 1, ref = NUM_CONFETTI; 1 <= ref ? j <= ref : j >= ref; i = 1 <= ref ? ++j : --j) {
+                        results.push(new Confetti);
+                    }
+                    return results;
+                })();
+
+                window.step = function() {
+                    var c, j, len, results;
+                    requestAnimationFrame(step);
+                    context.clearRect(0, 0, w, h);
+                    results = [];
+                    for (j = 0, len = confetti.length; j < len; j++) {
+                        c = confetti[j];
+                        results.push(c.draw());
+                    }
+                    return results;
+                };
+
+                step();
+
+            }).call(this);
 
 //二维码切换
 var $navApp = $(".nav-app"),
