44c78da649f9a97e8b0731314a2f940d47d7a259
zww张魏魏 <weiwzhang@ctrip.com>
2016-08-16 17:50:10 +0800

    更改导航交互

--- js/zw.js
+++ js/zw.js
@@ -6,12 +6,12 @@
                 } else {
                     $('.actGotop').fadeOut(300);
                 }
-                var p_top = 750;
-                if ($(window).scrollTop() > p_top) {
-                    $(".visible-md").addClass("person-fix");
-                } else {
-                    $(".visible-md").removeClass("person-fix");
-                }
+                //                var p_top = 750;
+                //                if ($(window).scrollTop() > p_top) {
+                //                    $(".visible-md").addClass("person-fix");
+                //                } else {
+                //                    $(".visible-md").removeClass("person-fix");
+                //                }
             });
             $('.actGotop').click(function () {
                 $('html,body').animate({
@@ -29,5 +29,33 @@
                 $(this).parents(".show-tips").addClass("ani-delay").removeClass("soccer-hide");
             });
 
+            //导航交互
+            var MQL = 1170;
+
+            //primary navigation slide-in effect
+
+            if ($(window).width() > MQL) {
+                var headerHeight = $('.navbar-custom').height();
+                $(window).on('scroll', {
+                        previousTop: 0
+                    },
+                    function () {
+                        var currentTop = $(window).scrollTop();
+                        //check if user is scrolling up
+                        if (currentTop < this.previousTop) {
+                            //if scrolling up...
+                            if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
+                                $('.navbar-custom').addClass('is-visible');
+                            } else {
+                                $('.navbar-custom').removeClass('is-visible is-fixed');
+                            }
+                        } else {
+                            //if scrolling down...
+                            $('.navbar-custom').removeClass('is-visible');
+                            if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
+                        }
+                        this.previousTop = currentTop;
+                    });
+            }
 
         })
