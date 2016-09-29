b4dd5192882a1215d5e4cd083c089d853eebf482
weiwzhang <weiwzhang@Ctrip.com>
2016-09-29 11:20:16 +0800

    测试demo

---
+++ demo/show/js/slide.js
@@ -0,0 +1,111 @@
+        $(document).ready(function () {
+
+            var intro = {
+                //初始化所有方法
+                init: function () {
+                    var webWidth = 300;
+                    if (window.screen.availWidth > webWidth) {
+                        this.slide();
+
+                        //                        this.fixNav();
+                    } else {
+                        console.log("手持设备不支持幻灯");
+                    };
+                    if ($(window).width() < 1030) {
+                        this.fClick();
+                    } else {
+                        return;
+                    };
+                },
+                //幻灯片
+                slide: function () {
+                    $(".demo").click(function () {
+                        clearInterval(timer);
+                        $(".slide-text").removeClass("slide-nav");
+                    })
+
+                    var timer;
+
+                    function changeIndex(clickBox) {
+                        //不需要点击
+                        if (clickBox == "div-banner") {
+                            return;
+
+                        } else {
+                            $('.' + clickBox + ' div .slide-text li').click(function () {
+
+                                clearInterval(timer);
+                                var a = $(this).index();
+                                $(this).addClass('curv').siblings('li').removeClass('curv');
+                                $('.' + clickBox + ' div div ul').eq(a).show().siblings('ul').hide();
+                                $('.' + clickBox + ' div div ul').eq(a).children('li').eq(0).addClass('cur1').show().siblings().removeClass("cur1").hide();
+                                slidePhone(clickBox);
+                                $(".slide-text").addClass("slide-nav");
+                            })
+                        }
+                    }
+
+                    function slidePhone(clickBoxs1) {
+
+                        var indexs = $('.' + clickBoxs1).children("div").children("ul").children(".curv").index();
+
+                        $('.' + clickBoxs1).children("div").children("ul").eq(indexs).show().siblings('ul').hide();
+                        //容器传参
+                        var as = $('.' + clickBoxs1).attr('class');
+                        //                                                console.log(as)
+
+                        function fun(div, indexs) {
+                            var index2 = $('.' + div + ' div div ul').eq(indexs).children('.cur1').index();
+                            var index1 = $('.' + div + ' div div ul').eq(indexs).children('li').length;
+                            var changeSlide = function change() {
+
+                                if (index2 < index1) {
+
+                                    index2 = index2 + 1;
+                                    if ($('.' + div + ' div div ul').eq(indexs).children('li').eq(index2 - 1).children('img').length > 1) {
+                                        $('.' + div + ' div div ul').eq(indexs).children('li').eq(index2 - 1).children('.img1').show();
+                                        $('.' + div + ' div div ul').eq(indexs).children('li').eq(index2 - 1).children('.img1').animate({
+                                            left: '-=20px',
+                                            top: '-=20px',
+                                            height: '40px',
+                                            width: '40px'
+                                        }, 700, function () {
+                                            $(this).hide()
+                                            $('.' + div + ' div div ul').eq(indexs).children('li').eq(index2).fadeIn(1000).siblings().fadeOut(1000);
+                                        });
+                                        $('.' + div + ' div div ul').eq(indexs).children('li').eq(index2 - 1).children('.img1').animate({
+                                            left: '+=20px',
+                                            top: '+=20px',
+                                            height: '0px',
+                                            width: '0px'
+                                        });
+                                        $('.' + div + ' div div ul').eq(indexs).children('li').eq(index2).addClass('cur1').siblings().removeClass("cur1");
+                                    } else {
+                                        $('.' + div + ' div div ul').eq(indexs).children('li').eq(index2).fadeIn(1000).siblings().fadeOut(1000);
+                                        $('.' + div + ' div div ul').eq(indexs).children('li').eq(index2).addClass('cur1').siblings().removeClass("cur1");
+                                    }
+                                } else {
+                                    //                                    index2 = 0;
+                                    //                                    $('.' + div + ' div div ul').eq(indexs).children('li').eq(index2).fadeIn(1000).siblings().fadeOut(1000);
+                                    //                                    $('.' + div + ' div div ul').eq(indexs).children('li').eq(index2).addClass('cur1').siblings().removeClass("cur1");
+                                    clearInterval(timer);
+
+                                }
+                            }
+                            timer = setInterval(changeSlide, 2000);
+                        }
+
+                        fun(as, indexs);
+                    }
+
+                    //行程幻灯
+                    changeIndex('div-apply');
+                    //出差申请幻灯
+                },
+                //fastclick
+                fClick: function () {
+                    FastClick.attach(document.body);
+                },
+            }
+            intro.init();
+        })
