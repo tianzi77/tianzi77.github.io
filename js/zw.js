fbc6d59674eafce53a384de639e1f48848f63bd4
weiwzhang <weiwzhang@Ctrip.com>
2017-07-26 15:45:59 +0800

    修复bug

--- js/zw.js
+++ js/zw.js
@@ -1,82 +1,82 @@
         $(function () {
 
-                //           $(window).scroll(function () {
-                //                if ($(window).scrollTop() >= 100) {
-                //                    $('.actGotop').fadeIn(300);
-                //                } else {
-                //                    $('.actGotop').fadeOut(300);
-                //                }
-                //                var p_top = 750;
-                //                if ($(window).scrollTop() > p_top) {
-                //                    $(".visible-md").addClass("person-fix");
-                //                } else {
-                //                    $(".visible-md").removeClass("person-fix");
-                //                }
-                //            });
-                //            $('.actGotop').click(function () {
-                //                $('html,body').animate({
-                //                    scrollTop: '0px'
-                //                }, 800);
-                //            });
-                //足球展开交互
+            //           $(window).scroll(function () {
+            //                if ($(window).scrollTop() >= 100) {
+            //                    $('.actGotop').fadeIn(300);
+            //                } else {
+            //                    $('.actGotop').fadeOut(300);
+            //                }
+            //                var p_top = 750;
+            //                if ($(window).scrollTop() > p_top) {
+            //                    $(".visible-md").addClass("person-fix");
+            //                } else {
+            //                    $(".visible-md").removeClass("person-fix");
+            //                }
+            //            });
+            //            $('.actGotop').click(function () {
+            //                $('html,body').animate({
+            //                    scrollTop: '0px'
+            //                }, 800);
+            //            });
+            //足球展开交互
 
-                $(".t-img").on("click", function () {
-                    $(this).parents(".show-tips").removeClass("ani-delay").addClass("soccer-hide");
-                    $(this).siblings(".tools").addClass("tools-show");
-                });
-                $(".t-close").on("click", function () {
-                    $(this).parents(".tools").removeClass("tools-show");
-                    $(this).parents(".show-tips").addClass("ani-delay").removeClass("soccer-hide");
-                });
+            $(".t-img").on("click", function () {
+                $(this).parents(".show-tips").removeClass("ani-delay").addClass("soccer-hide");
+                $(this).siblings(".tools").addClass("tools-show");
+            });
+            $(".t-close").on("click", function () {
+                $(this).parents(".tools").removeClass("tools-show");
+                $(this).parents(".show-tips").addClass("ani-delay").removeClass("soccer-hide");
+            });
 
-                //导航交互
-                var MQL = 1170;
+            //导航交互
+            var MQL = 1170;
 
-                //primary navigation slide-in effect
+            //primary navigation slide-in effect
 
-                if ($(window).width() > MQL) {
-                    var headerHeight = $('.navbar-custom').height();
-                    $(window).on('scroll', {
-                            previousTop: 0
-                        },
-                        function () {
-                            var currentTop = $(window).scrollTop();
-                            //check if user is scrolling up
-                            if (currentTop < this.previousTop) {
-                                //if scrolling up...
-                                if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
-                                    $('.navbar-custom').addClass('is-visible');
-                                } else {
-                                    $('.navbar-custom').removeClass('is-visible is-fixed');
-                                }
+            if ($(window).width() > MQL) {
+                var headerHeight = $('.navbar-custom').height();
+                $(window).on('scroll', {
+                        previousTop: 0
+                    },
+                    function () {
+                        var currentTop = $(window).scrollTop();
+                        //check if user is scrolling up
+                        if (currentTop < this.previousTop) {
+                            //if scrolling up...
+                            if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
+                                $('.navbar-custom').addClass('is-visible');
                             } else {
-                                //if scrolling down...
-                                $('.navbar-custom').removeClass('is-visible');
-                                if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
+                                $('.navbar-custom').removeClass('is-visible is-fixed');
                             }
-                            this.previousTop = currentTop;
-                        });
-                }
+                        } else {
+                            //if scrolling down...
+                            $('.navbar-custom').removeClass('is-visible');
+                            if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
+                        }
+                        this.previousTop = currentTop;
+                    });
+            }
 
-                //css动画抖动
-                var windowH = $(window).height(),
-                    bodyH = $("body").height();
-                if (bodyH <= windowH) {
-                    $("body").addClass("overflow");
-                } else {
-                    $("body").removeClass("overflow");
-                }
-                // 首页背景
+            //css动画抖动
+            var windowH = $(window).height(),
+                bodyH = $("body").height();
+            if (bodyH <= windowH) {
+                $("body").addClass("overflow");
+            } else {
+                $("body").removeClass("overflow");
+            }
+            // 首页背景
+            fixBg();
+            $(window).resize(function () {
                 fixBg();
-                $(window).resize(function () {
-                    fixBg();
-                })
+            })
 
-                function fixBg() {
-                    var win = $(window).width(),
-                        winH = $(window).height();
-                    if (win > 1024) {
-                        $('#home-bg').css('height', winH);
-                    }
+            function fixBg() {
+                var win = $(window).width(),
+                    winH = $(window).height();
+                if (win > 1024) {
+                    $('#home-bg').css('height', winH);
                 }
-        }
+            }
+        })
