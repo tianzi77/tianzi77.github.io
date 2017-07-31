32343ca4ec9cdb94332d1d51f3366df722a55e9a
weiwzhang <weiwzhang@Ctrip.com>
2017-07-31 14:17:41 +0800

    修改

--- js/zw.js
+++ js/zw.js
@@ -1,91 +1,3 @@
-        $(function () {
-
-            //           $(window).scroll(function () {
-            //                if ($(window).scrollTop() >= 100) {
-            //                    $('.actGotop').fadeIn(300);
-            //                } else {
-            //                    $('.actGotop').fadeOut(300);
-            //                }
-            //                var p_top = 750;
-            //                if ($(window).scrollTop() > p_top) {
-            //                    $(".visible-md").addClass("person-fix");
-            //                } else {
-            //                    $(".visible-md").removeClass("person-fix");
-            //                }
-            //            });
-            //            $('.actGotop').click(function () {
-            //                $('html,body').animate({
-            //                    scrollTop: '0px'
-            //                }, 800);
-            //            });
-            //足球展开交互
-
-            $(".t-img").on("click", function () {
-                $(this).parents(".show-tips").removeClass("ani-delay").addClass("soccer-hide");
-                $(this).siblings(".tools").addClass("tools-show");
-            });
-            $(".t-close").on("click", function () {
-                $(this).parents(".tools").removeClass("tools-show");
-                $(this).parents(".show-tips").addClass("ani-delay").removeClass("soccer-hide");
-            });
-
-            //导航交互
-            var MQL = 1170;
-
-            //primary navigation slide-in effect
-
-            if ($(window).width() > MQL) {
-                $("body").append('<div id="backtoTop" data-action="gototop"><canvas id="backtoTopCanvas" width="48" height="48"></canvas><div class="per"></div></div>');
-
-                var T = bigfa_scroll;
-
-                T.backToTop($("#backtoTop"));
-
-                T.scrollHook($(window), "#FF5E52");
-                var headerHeight = $('.navbar-custom').height();
-                $(window).on('scroll', {
-                        previousTop: 0
-                    },
-                    function () {
-                        var currentTop = $(window).scrollTop();
-                        //check if user is scrolling up
-                        if (currentTop < this.previousTop) {
-                            //if scrolling up...
-                            if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
-                                $('.navbar-custom').addClass('is-visible');
-                            } else {
-                                $('.navbar-custom').removeClass('is-visible is-fixed');
-                            }
-                        } else {
-                            //if scrolling down...
-                            $('.navbar-custom').removeClass('is-visible');
-                            if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
-                        }
-                        this.previousTop = currentTop;
-                    });
-            }
-
-            //css动画抖动
-            var windowH = $(window).height(),
-                bodyH = $("body").height();
-            if (bodyH <= windowH) {
-                $("body").addClass("overflow");
-            } else {
-                $("body").removeClass("overflow");
-            }
-            // 首页背景
-            //fixBg();
-            $(window).resize(function () {
-                //fixBg();
-            })
-
-            function fixBg() {
-                var win = $(window).width(),
-                    winH = $(window).height();
-                if (win > 1024) {
-                    $('#home-bg').css('height', winH);
-                }
-            }
             //返回顶部
 
             var bigfa_scroll = {
@@ -209,4 +121,94 @@
                 }
 
             }
-        })
+            $(function () {
+
+                //           $(window).scroll(function () {
+                //                if ($(window).scrollTop() >= 100) {
+                //                    $('.actGotop').fadeIn(300);
+                //                } else {
+                //                    $('.actGotop').fadeOut(300);
+                //                }
+                //                var p_top = 750;
+                //                if ($(window).scrollTop() > p_top) {
+                //                    $(".visible-md").addClass("person-fix");
+                //                } else {
+                //                    $(".visible-md").removeClass("person-fix");
+                //                }
+                //            });
+                //            $('.actGotop').click(function () {
+                //                $('html,body').animate({
+                //                    scrollTop: '0px'
+                //                }, 800);
+                //            });
+                //足球展开交互
+
+                $(".t-img").on("click", function () {
+                    $(this).parents(".show-tips").removeClass("ani-delay").addClass("soccer-hide");
+                    $(this).siblings(".tools").addClass("tools-show");
+                });
+                $(".t-close").on("click", function () {
+                    $(this).parents(".tools").removeClass("tools-show");
+                    $(this).parents(".show-tips").addClass("ani-delay").removeClass("soccer-hide");
+                });
+
+                //导航交互
+                var MQL = 1170;
+
+                //primary navigation slide-in effect
+
+                if ($(window).width() > MQL) {
+                    var headerHeight = $('.navbar-custom').height();
+                    $(window).on('scroll', {
+                            previousTop: 0
+                        },
+                        function () {
+                            var currentTop = $(window).scrollTop();
+                            //check if user is scrolling up
+                            if (currentTop < this.previousTop) {
+                                //if scrolling up...
+                                if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
+                                    $('.navbar-custom').addClass('is-visible');
+                                } else {
+                                    $('.navbar-custom').removeClass('is-visible is-fixed');
+                                }
+                            } else {
+                                //if scrolling down...
+                                $('.navbar-custom').removeClass('is-visible');
+                                if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
+                            }
+                            this.previousTop = currentTop;
+                        });
+                }
+
+                //css动画抖动
+                var windowH = $(window).height(),
+                    bodyH = $("body").height();
+                if (bodyH <= windowH) {
+                    $("body").addClass("overflow");
+                } else {
+                    $("body").removeClass("overflow");
+                }
+                // 首页背景
+                //fixBg();
+                $(window).resize(function () {
+                    //fixBg();
+                })
+
+                function fixBg() {
+                    var win = $(window).width(),
+                        winH = $(window).height();
+                    if (win > 1024) {
+                        $('#home-bg').css('height', winH);
+                    }
+                }
+                if ($(window).width() > 980) {
+                    $("body").append('<div id="backtoTop" data-action="gototop"><canvas id="backtoTopCanvas" width="48" height="48"></canvas><div class="per"></div></div>');
+
+                    var T = bigfa_scroll;
+
+                    T.backToTop($("#backtoTop"));
+
+                    T.scrollHook($(window), "#FF5E52");
+                }
+            })
